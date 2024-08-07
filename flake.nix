3271fc2f2831d21c45d4f633ccab268a59bf76a7
Dillon Beliveau <dillonbeliveau@gmail.com>
2024-08-06 23:44:52 -0700

    Initial commit

---
+++ flake.nix
@@ -0,0 +1,21 @@
+{
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+
+  outputs = { self, nixpkgs }:
+    let
+      supportedSystems = [ "x86_64-linux" "x86_64-darwin" "aarch64-linux" "aarch64-darwin" ];
+      forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+      pkgs = forAllSystems (system: nixpkgs.legacyPackages.${system});
+    in
+    {
+      devShells = forAllSystems (system: {
+        default = pkgs.${system}.mkShell {
+          buildInputs = [
+            pkgs.${system}.python312
+            pkgs.${system}.python312Packages.bleak
+            pkgs.${system}.python312Packages.requests
+          ];
+        };
+      });
+    };
+}
---
+++ scan.py
@@ -0,0 +1,151 @@
+#!/usr/bin/env python3
+
+# Research from:
+# Neither source was completely correct, this script is a combination of knowledge from both
+# https://www.cnblogs.com/wstong2052/p/17767538.html
+# https://github.com/oliexdev/openScale/blob/master/android_app/app/src/main/java/com/health/openscale/core/bluetooth/BluetoothQNScale.java
+
+import asyncio
+import argparse
+from bleak import BleakClient, BleakGATTCharacteristic
+from bleak.exc import BleakDeviceNotFoundError
+import time
+import requests
+
+parser = argparse.ArgumentParser(
+        prog="scale-recorder",
+        description="Records data from a very specific BLE scale and sends it to the console, Discord, and a sqlite db."
+        )
+
+
+parser.add_argument('-d', '--discord-webhook-file', type=str)
+parser.add_argument('-s', '--sqlite-path', type=str)
+
+args = parser.parse_args()
+
+hook = None
+
+if args.discord_webhook_file is None:
+    print("Not sending data to Discord.")
+else:
+    hook = open(args.discord_webhook_file, "r").read().strip()
+    print(f"Will send data to discord through webhook: '{hook}'")
+
+def send_discord_message(msg):
+    if hook is None:
+        print("No webhook, not sending message!")
+        print(f"Message: {msg}")
+    else:
+        msg_json = {
+                'username': "Scale",
+                'content': msg
+                }
+        requests.post(hook, json = msg_json)
+
+if args.sqlite_path is None:
+    print("Not saving data to sqlite")
+
+class StabilizationTimeoutException(Exception):
+    pass
+
+def checksum(l):
+    c = 0
+    for x in l:
+        c += x
+        c &= 0xFF
+    return c
+
+address = "FF:03:00:38:F5:0B"
+read_service = "0000fff1-0000-1000-8000-00805f9b34fb"
+write_service = "0000fff2-0000-1000-8000-00805f9b34fb"
+
+# 0x01 == kilograms 0x02 == pounds
+# This byte appears to only affect the data displayed on screen, it has no effect on the data sent back over BLE.
+unit_byte = 0x02
+
+def kg_to_lb(kg):
+    return round(kg * 2.20462, 1)
+
+hello_magic = [0x13, 0x09, 0x15, unit_byte, 0x10, 0x00, 0x00, 0x00, 0x00]
+hello_magic.append(checksum(hello_magic))
+
+timestamp_int = int(time.time()) - 946702800 # time before 2000 did not exist
+timestamp_bytes = [
+        0x02,
+        (timestamp_int >>  0) & 0xFF,
+        (timestamp_int >>  8) & 0xFF,
+        (timestamp_int >> 16) & 0xFF,
+        (timestamp_int >> 32) & 0xFF,
+        ]
+
+goodbye_magic = [0x1F, 0x05, 0x21, 0x10, 0x49]
+
+def print_hex(bytes):
+    l = [hex(int(i)) for i in bytes]
+    return " ".join(l)
+
+def get_int16(data, index):
+    a = data[index]
+    b = data[index + 1]
+    return (((a & 0xFF) << 8) + (b & 0xFF))
+
+weight = 0
+weight_kg = 0
+weight_lb = 0
+
+def callback(sender: BleakGATTCharacteristic, data: bytearray):
+    global weight
+    global weight_kg
+    global weight_lb
+
+    status = data[5]
+    if status == 0:
+        temp_weight = get_int16(data, 3) / 100
+        temp_weight_lb = kg_to_lb(temp_weight)
+        print(f"Weight still unsteady, hold on... {temp_weight_lb}lb")
+    elif status == 1:
+        weight = get_int16(data, 3)
+        weight_kg = weight / 100
+        weight_lb = kg_to_lb(weight_kg)
+    else:
+        print(f"Unknown status byte value {status:X}: data = {print_hex(data)}")
+
+
+async def main(address):
+    global weight
+    weight = 0
+    print("Waiting for scale to appear...")
+    async with BleakClient(address) as client:
+        await client.start_notify(read_service, callback)
+
+        await client.write_gatt_char(write_service, bytes(hello_magic))
+        await client.write_gatt_char(write_service, bytes(timestamp_bytes))
+
+        timeout = 10
+        while weight == 0 and timeout > 0:
+            await asyncio.sleep(1.0)
+            timeout -= 1
+
+        if timeout == 0:
+            raise StabilizationTimeoutException
+
+        await client.write_gatt_char(write_service, bytes(goodbye_magic))
+
+        print(f"Weight: {weight_lb}lb")
+        send_discord_message(f"Weight: Raw: {weight} {weight_kg}kg {weight_lb}lb")
+
+
+while True:
+    try:
+        asyncio.run(main(address))
+        print("Waiting 10 seconds before polling again")
+        time.sleep(10)
+    except BleakDeviceNotFoundError:
+        print("Device not found, polling again...")
+    except StabilizationTimeoutException:
+        print("Timed out waiting for stabilization, starting over...")
+    except Exception as e:
+        print(f"Error, {e}")
+        print("Waiting 10 seconds before polling again")
+        time.sleep(10)
+
