f8a70e5f0e8a5d150a4fe6aa1a3de2c5235b1d7c
Dillon Beliveau <dillonbeliveau@gmail.com>
2024-08-07 20:25:17 -0700

    Rename script, add package and app to Flake

--- flake.nix
+++ flake.nix
@@ -5,9 +5,32 @@
     let
       supportedSystems = [ "x86_64-linux" "x86_64-darwin" "aarch64-linux" "aarch64-darwin" ];
       forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+      shortRev = with self; if sourceInfo?dirtyShortRev then sourceInfo.dirtyShortRev else sourceInfo.shortRev;
       pkgs = forAllSystems (system: nixpkgs.legacyPackages.${system});
     in
     {
+      packages = forAllSystems (system: {
+        default = pkgs.${system}.stdenv.mkDerivation {
+          name = "ble-scale";
+          version = shortRev;
+          dontUnpack = true;
+          buildInputs = with pkgs.${system}; [
+            (python312.withPackages (pythonPackages: with pythonPackages; [
+              bleak
+              requests
+            ]))
+          ];
+          installPhase = "install -Dm755 ${./ble-scale.py} $out/bin/ble-scale";
+        };
+      });
+
+      apps = forAllSystems (system: {
+        default = {
+          type = "app";
+          program = "${self.packages.${system}.default}/bin/ble-scale";
+        };
+      });
+
       devShells = forAllSystems (system: {
         default = pkgs.${system}.mkShell {
           buildInputs = with pkgs.${system}; [
